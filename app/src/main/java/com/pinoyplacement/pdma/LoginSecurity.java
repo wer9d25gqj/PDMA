a76bbadec5a10fec679629c40454e07be3cb63b1
John Lomabao <johnrey_l@pinoydata.com>
2016-04-13 12:26:39 +0800

    New Class for PDMA user properties

---
+++ app/src/main/java/com/pinoyplacement/pdma/LoginSecurity.java
@@ -0,0 +1,8 @@
+package com.pinoyplacement.pdma;
+
+/**
+ * Created by Admin on 4/13/2016.
+ */
+public class LoginSecurity {
+
+}
---
+++ app/src/main/java/com/pinoyplacement/pdma/UserCls.java
@@ -0,0 +1,12 @@
+package com.pinoyplacement.pdma;
+
+/**
+ * Created by Admin on 4/13/2016.
+ */
+public class UserCls {
+    public String UserID;
+
+    public UserCls(String UserID) {
+        this.UserID = UserID;
+    }
+}
